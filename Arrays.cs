39acf66be75c2ff2c1c4d3734e190095f9b44efa
ReneUnam <rene.sandoval23908226@estu.unan.edu.ni>
2023-10-27 21:42:52 -0600

    Hola profe

---
+++ Arrays.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+class Arrays
+{
+    static void Main (string [] args)
+    {
+        int cont = 0;
+
+        System.Console.WriteLine("Defina el tamaño del arreglo");
+
+        int a = int.Parse(System.Console.ReadLine());
+
+        int[] arreglo = new int[a];
+
+        for (int i = 0; i < a; i++)
+        {
+            System.Console.WriteLine("Ingrese el valor " + (i + 1));
+
+            arreglo [i] = int.Parse(System.Console.ReadLine());
+
+            cont = cont + arreglo[i];
+
+            System.Console.WriteLine($"La posicion {i + 1} equivale a {cont}\n");
+
+        }     
+
+        System.Console.WriteLine("La suma de los arreglos es {0}", cont );
+
+        Console.ReadKey();
+
+
+    }
+}
