f3e3ec8b61d2d344013269560489d7342f12f043
ReneUnam <rene.sandoval23908226@estu.unan.edu.ni>
2024-05-30 13:53:03 -0600

    akdajdjaa

--- CapaPresentacion/FrmUsuarios.Designer.cs
+++ CapaPresentacion/FrmUsuarios.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuarios));
             this.SuspendLayout();
             // 
             // FrmUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1282, 762);
+            this.ClientSize = new System.Drawing.Size(1282, 732);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmUsuarios";
-            this.Text = "FrmUsuarios";
+            this.Text = "VAND y MONTI";
             this.ResumeLayout(false);
 
         }
