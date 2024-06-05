a6f931818f680c62890464f46b97b93d67ffbe66
ReneUnam <rene.sandoval23908226@estu.unan.edu.ni>
2024-06-05 15:39:35 -0600

    sisisisi

--- CapaPresentacion/FrmPrincipal.Designer.cs
+++ CapaPresentacion/FrmPrincipal.Designer.cs
@@ -40,6 +40,7 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button8 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -55,10 +56,9 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(0, 86);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(447, 845);
+            this.panel1.Size = new System.Drawing.Size(401, 1418);
             this.panel1.TabIndex = 0;
             // 
             // button7
@@ -72,7 +72,7 @@
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button7.Location = new System.Drawing.Point(0, 612);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(447, 117);
+            this.button7.Size = new System.Drawing.Size(401, 117);
             this.button7.TabIndex = 7;
             this.button7.Text = "       Cerrar Sesion";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -89,7 +89,7 @@
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.Location = new System.Drawing.Point(0, 510);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(447, 102);
+            this.button6.Size = new System.Drawing.Size(401, 102);
             this.button6.TabIndex = 6;
             this.button6.Text = "       Ajustes";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -106,7 +106,7 @@
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.Location = new System.Drawing.Point(0, 408);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(447, 102);
+            this.button5.Size = new System.Drawing.Size(401, 102);
             this.button5.TabIndex = 5;
             this.button5.Text = "       Usuarios";
             this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -124,7 +124,7 @@
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.Location = new System.Drawing.Point(0, 306);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(447, 102);
+            this.button4.Size = new System.Drawing.Size(401, 102);
             this.button4.TabIndex = 4;
             this.button4.Text = "       Reportes";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -141,7 +141,7 @@
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.Location = new System.Drawing.Point(0, 204);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(447, 102);
+            this.button3.Size = new System.Drawing.Size(401, 102);
             this.button3.TabIndex = 3;
             this.button3.Text = "       Historial";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -158,7 +158,7 @@
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.Location = new System.Drawing.Point(0, 102);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(447, 102);
+            this.button2.Size = new System.Drawing.Size(401, 102);
             this.button2.TabIndex = 2;
             this.button2.Text = "       Nueva factura";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -175,7 +175,7 @@
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(447, 102);
+            this.button1.Size = new System.Drawing.Size(401, 102);
             this.button1.TabIndex = 1;
             this.button1.Text = "       Productos";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -184,42 +184,60 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(91)))), ((int)(((byte)(178)))));
-            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button8);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(447, 0);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(835, 88);
+            this.panel2.Size = new System.Drawing.Size(1282, 88);
             this.panel2.TabIndex = 2;
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Black", 15.14286F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(2, 18);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(91)))), ((int)(((byte)(178)))));
+            this.label2.Location = new System.Drawing.Point(601, 162);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(230, 48);
             this.label2.TabIndex = 9;
             this.label2.Text = "Bienvenido ";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(609, 251);
+            this.pictureBox1.Location = new System.Drawing.Point(609, 225);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(496, 469);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Segoe UI Black", 15.14286F, System.Drawing.FontStyle.Bold);
+            this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
+            this.button8.Image = global::CapaPresentacion.Properties.Resources.menu_hamburguesa__1_;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.Location = new System.Drawing.Point(0, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(401, 88);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "       Menú";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1282, 845);
+            this.ClientSize = new System.Drawing.Size(1282, 829);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
@@ -231,9 +249,9 @@
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -250,5 +268,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
     }
 }
--- CapaPresentacion/FrmPrincipal.cs
+++ CapaPresentacion/FrmPrincipal.cs
@@ -69,9 +69,6 @@ namespace CapaPresentacion
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
