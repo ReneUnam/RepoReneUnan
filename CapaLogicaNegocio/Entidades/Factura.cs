34a1152f63b3a77bde27345bd6921cc3f4581024
ReneUnam <rene.sandoval23908226@estu.unan.edu.ni>
2024-06-07 20:59:41 -0600

    agregado factura  y producto

---
+++ CapaLogicaNegocio/Entidades/Factura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaNegocio.Entidades
+{
+    internal class Factura
+    {
+        private int idFactura { get; set; }
+
+        public string NombreUsuario { get; set;}
+
+        public DateTime Fecha { get; set;}
+
+        public double Iva { get; set;}
+
+        public double Total { get; set;}
+
+        public int idUsuario { get; set;}
+
+        public int idProveedor { get; set;}
+    }
+}
---
+++ CapaLogicaNegocio/Entidades/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaNegocio.Entidades
+{
+    internal class Producto
+    {
+        public int idProductos { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int IdCategoria { get; set;}
+
+        public double PrecioVenta { get; set;}
+
+        public int Stock { get; set;}
+
+        public DateTime FechaVencimiento { get; set; }
+
+    }
+}
