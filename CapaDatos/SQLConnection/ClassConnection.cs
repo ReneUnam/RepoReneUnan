e8138a05cd1132bc50f4f7cf583900360da4985c
Garyjnm <garyn7674@gmail.com>
2024-06-19 13:30:56 -0600

    nose

--- CapaDatos/SQLConnection/ClassConnection.cs
+++ CapaDatos/SQLConnection/ClassConnection.cs
@@ -15,7 +15,7 @@ namespace CapaDatos.SQLConnection
         public ClassConnection()
         {
             //NOTA: Si van a cambiar a su base de dato simplemente copien y peguen aquí.
-            ConnectionToString = ("Data Source=ReneAlexander\\SQLEXPRESS;Initial Catalog=Vand_Y_Monti;Integrated Security=True");
+            ConnectionToString = ("Data Source=GARY\\MYSQL;Initial Catalog=Vand_Y_Monti;Integrated Security=True;TrustServerCertificate=True");
         }
         protected SqlConnection GetConnection() 
         {
